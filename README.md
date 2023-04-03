4d5a442f245356dbe2017a278475ee867f1d67fc
seafoodude <michaelhuang777@gmail.com>
2023-04-02 17:47:49 -0700

    finished the functions in index.js, now need to wrap up genearteMarkdown with rendering the license

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
-# Homework-7-README
+# fdsa
+
+  ## Description
+
+  fdsa
+
+  ## Table of Contents
+
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+
+  ## Installation
+
+  fdsa
+
+  ## Usage
+
+  fdsa
+
+  ## License
+
+  undefined
+
+  undefined
+
+  undefined
+
+  ## Contributing
+
+  fdsa
+
+  ## Tests
+
+  fdsa
+
+  ## Questions
+
+  For additional questions, please contact fdsa. Visit my [GitHub profile](https://github.com/undefined) to see my other projects.
+  
--- index.js
+++ index.js
@@ -1,13 +1,80 @@
 // TODO: Include packages needed for this application
 
+const inquirer = require("inquirer");
+const fs = require('fs');
+const util = require("util");
+
+const generatorMarkdown = require('./utils/generateMarkdown');
+
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [{
+    type: "input",
+    message: "What is the title of your project?",
+    name:"title"
+}, {
+    type: "input",
+    message: "What is the project about? Give a detailed description of your project.",
+    name:"description"
+}, {
+    type: "input",
+    message: "Table of Contents.",
+    name:"table of contents"
+}, {
+    type: "input",
+    message: "What does the user need to install to run this app?",
+    name:"installation"
+}, {
+    type: "input",
+    message: "How is the app used? Give instructions.",
+    name:"usage"
+}, {
+    type: "input",
+    message: "What license is being used?",
+    name:"license"
+}, {
+    type: "input",
+    message: "Who contributed to this project?:",
+    name:"contributing"
+}, {
+    type: "input",
+    message: "What commands are needed to test this app?",
+    name:"tests"
+}, {
+    type: "input",
+    message: "Contact info for inquiries",
+    name:"questions"
+}, {
+    type: 'input',
+    message: 'What is your Github username?',
+    name:'username'
+}, {
+    type: 'input',
+    message: 'What is your email address?',
+    name:'email'
+}
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, function(err) {
+        console.log(fileName);
+        console.log(data);
+        if (err) {
+            return console.log(err);
+        } else {
+            console.log("success!");
+        }
+    })
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+    .then(function(data) {
+        writeToFile("README.md", generatorMarkdown(data));
+        console.log(data);
+    })
+}
 
 // Function call to initialize app
 init();
