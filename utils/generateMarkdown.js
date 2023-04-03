373ad658c47fcae733148abc59d648bed2f8178e
seafoodude <michaelhuang777@gmail.com>
2023-04-02 17:18:05 -0700

    added readme into function generate markdown and moved files around

---
+++ utils/generateMarkdown.js
@@ -0,0 +1,61 @@
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+function renderLicenseBadge(license) {}
+
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) {}
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {}
+
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(data) {
+  return `# ${data.title}
+
+  ## Description
+
+  ${data.description}
+
+  ## Table of Contents
+
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+
+  ## Installation
+
+  ${data.installation}
+
+  ## Usage
+
+  ${data.usage}
+
+  ## License
+
+  ${renderLicenseBadge(data.license)}
+
+  ${renderLicenseLink(data.license)}
+
+  ${renderLicenseSection(data.license)}
+
+  ## Contributing
+
+  ${data.contributing}
+
+  ## Tests
+
+  ${data.tests}
+
+  ## Questions
+
+  For additional questions, please contact ${data.email}. Visit my [GitHub profile](https://github.com/${data.github}) to see my other projects.
+  
+`;
+}
+
+module.exports = generateMarkdown;
